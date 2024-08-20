e3019fd5d771d11144c47410b2c77b146d207865
Marina <marina.lsz522@gmail.com>
2024-08-20 18:02:44 +0200

    Terminado 1 y 2

---
+++ ejercicio2.js
@@ -0,0 +1,50 @@
+//Ejercicio 2
+
+const users = [
+  {
+    name: "Alberto",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Antonio",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Santiago",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Laura",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+let totalVolume = 0;
+let totalSounds = 0;
+
+for (const user of users) {
+    for (const sound in user.favoritesSounds) {
+        totalVolume += user.favoritesSounds[sound].volume;
+        totalSounds++;
+    }
+}
+
+const averageVolume = totalVolume / totalSounds;
+
+console.log("La media de volumen es: " + averageVolume);
